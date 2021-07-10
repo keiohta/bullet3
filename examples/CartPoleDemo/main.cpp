d7d6e062bccf160f45c6991dd2eeea15b6927ccd
Kei Ohta <dev.ohtakei@gmail.com>
2021-07-10 16:19:54 +0900

    trying to connect by DIRECT method but fails

--- examples/CartPoleDemo/main.cpp
+++ examples/CartPoleDemo/main.cpp
@@ -5,11 +5,15 @@
 #include <stdio.h>
 
 #include "../SharedMemory/SharedMemoryPublic.h"
+#include "../SharedMemory/PhysicsClientC_API.h"
+#include "../SharedMemory/PhysicsDirect.h"
 
 int main(int argc, char* argv[])
 {
     int method = eCONNECT_DIRECT;
 	printf("method = %d\n", method);
+
+	b3PhysicsClientHandle sm = b3ConnectPhysicsDirect();
 
 	return 0;
 }
