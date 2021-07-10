ebcadcb7eb3c2996f7f6991dd059a544fb59ad03
Kei Ohta <dev.ohtakei@gmail.com>
2021-07-10 17:21:57 +0900

    feat: successfully load cartpole urdf file

--- examples/CartPoleDemo/main.cpp
+++ examples/CartPoleDemo/main.cpp
@@ -10,11 +10,28 @@
 
 int main(int argc, char* argv[])
 {
-    int method = eCONNECT_DIRECT;
+	int method = eCONNECT_DIRECT;
 	printf("method = %d\n", method);
 
-	b3PhysicsClientHandle sm;
-	sm = b3ConnectPhysicsDirect();
+	// Directly connect to physics engine
+	b3PhysicsClientHandle sm = b3ConnectPhysicsDirect();
+
+	// Load CartPole URDF model
+	int flags = 0;
+	const char* urdfFileName = "../../build_cmake/examples/pybullet/pybullet_data/cartpole.urdf";
+	b3SharedMemoryCommandHandle command = b3LoadUrdfCommandInit(sm, urdfFileName);
+	b3LoadUrdfCommandSetFlags(command, flags);
+	printf("Loading %s...\n", urdfFileName);
+	b3SharedMemoryStatusHandle statusHandle = b3SubmitClientCommandAndWaitStatus(sm, command);
+	int statusType = b3GetStatusType(statusHandle);
+	if (statusType != CMD_URDF_LOADING_COMPLETED)
+	{
+		printf("Status type is %d, not %d. Cannot load URDF file.\n",
+			   statusType, int(CMD_URDF_LOADING_COMPLETED));
+		return -1;
+	}
+	int bodyUniqueId = b3GetStatusBodyIndex(statusHandle);
+	printf("The bodyUniqueID is %d\n", bodyUniqueId);
 
 	return 0;
 }
