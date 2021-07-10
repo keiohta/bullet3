b237c5d567ad9a8d02f1412f38000642f760d36f
Kei Ohta <dev.ohtakei@gmail.com>
2021-07-10 16:09:38 +0900

    print the connection method

--- examples/CartPoleDemo/main.cpp
+++ examples/CartPoleDemo/main.cpp
@@ -2,11 +2,14 @@
 // Created by ohta on 2021/07/10.
 //
 
+#include <stdio.h>
+
 #include "../SharedMemory/SharedMemoryPublic.h"
 
 int main(int argc, char* argv[])
 {
     int method = eCONNECT_DIRECT;
+	printf("method = %d\n", method);
 
 	return 0;
 }
