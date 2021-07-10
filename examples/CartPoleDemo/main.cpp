6783f491192b28c97f2940226a368c7b3728011b
Kei Ohta <dev.ohtakei@gmail.com>
2021-07-10 16:04:29 +0900

    define connection method to be DIRECT

--- examples/CartPoleDemo/main.cpp
+++ examples/CartPoleDemo/main.cpp
@@ -1,3 +1,12 @@
 //
 // Created by ohta on 2021/07/10.
 //
+
+#include "../SharedMemory/SharedMemoryPublic.h"
+
+int main(int argc, char* argv[])
+{
+    int method = eCONNECT_DIRECT;
+
+	return 0;
+}
